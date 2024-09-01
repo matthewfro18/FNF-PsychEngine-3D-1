a46bf9bc8552e72a97bad98f07e08764e534eb85
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-08-31 20:30:13 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -11,7 +11,7 @@ haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
 haxelib install hscript-iris 1.0.2
 haxelib install tjson 1.4.0
-haxelib install away3d
+haxelib git away3d https://github.com/openfl/away3d 845ffa489ecc35397d4dee917d0de4e9ea0dd4c0
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 633fcc051399afed6781dd60cbf30ed8c3fe2c5a
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc 3538a1c2bb07b04208cd014220207f8173acdb21
