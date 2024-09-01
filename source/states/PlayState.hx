271cce7eb79294d035804ea3397957b28d22105c
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-08-31 20:13:08 -0400

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -363,6 +363,7 @@ class PlayState extends MusicBeatState
 			case 'tank': new Tank();					//Week 7 - Ugh, Guns, Stress
 			case 'phillyStreets': new PhillyStreets(); 	//Weekend 1 - Darnell, Lit Up, 2Hot
 			case 'phillyBlazin': new PhillyBlazin();	//Weekend 1 - Blazin
+			case 'tvstage': new TVStage();
 		}
 		if(isPixelStage) introSoundsSuffix = '-pixel';
 
